b80e17615b478c2d6023a8e4571da6c33f4b758b
khuong2924 <truongkhuong666@gmail.com>
2025-01-19 23:28:00 +0700

    starter config kafka

---
+++ src/main/java/khuong/com/smartorderbeorderdomain/configs/KafkaTopicConfig.java
@@ -0,0 +1,37 @@
+package khuong.com.smartorderbeorderdomain.configs;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    @Bean
+    public AdminClient adminClient() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return AdminClient.create(configs);
+    }
+
+    @Bean
+    public NewTopic createTopic() throws ExecutionException, InterruptedException {
+        NewTopic topic = new NewTopic(topicName, 1, (short) 1);
+        adminClient().createTopics(Collections.singletonList(topic)).all().get();
+        return topic;
+    }
+}
