8410834a4b4e9e4a22fed923a020bc1180a16091
khuong2924 <truongkhuong666@gmail.com>
2025-02-08 01:12:03 +0700

    order controller

---
+++ src/main/java/khuong/com/smartorderbeorderdomain/order/controller/OrderController.java
@@ -0,0 +1,29 @@
+
+package khuong.com.smartorderbeorderdomain.order.controller;
+
+import khuong.com.smartorderbeorderdomain.order.dto.request.CreateOrderRequest;
+import khuong.com.smartorderbeorderdomain.order.dto.response.OrderResponse;
+import khuong.com.smartorderbeorderdomain.order.enums.OrderStatus;
+import khuong.com.smartorderbeorderdomain.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> createOrder(@RequestBody CreateOrderRequest request) {
+        OrderResponse orderResponse = orderService.createOrder(request);
+        return ResponseEntity.ok(orderResponse);
+    }
+
+    @PutMapping("/{orderId}/status")
+    public ResponseEntity<OrderResponse> updateOrderStatus(@PathVariable String orderId, @RequestParam OrderStatus status) {
+        OrderResponse orderResponse = orderService.updateOrderStatus(orderId, status);
+        return ResponseEntity.ok(orderResponse);
+    }
+}
