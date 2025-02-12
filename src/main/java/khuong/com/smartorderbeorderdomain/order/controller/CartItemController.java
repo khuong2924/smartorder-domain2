74fd840278ac90df14257f5670db1508caabdee0
khuong2924 <truongkhuong666@gmail.com>
2025-02-12 20:59:25 +0700

    config cartitems serr

---
+++ src/main/java/khuong/com/smartorderbeorderdomain/order/controller/CartItemController.java
@@ -0,0 +1,45 @@
+package khuong.com.smartorderbeorderdomain.order.controller;
+
+
+import khuong.com.smartorderbeorderdomain.order.entity.CartItem;
+import khuong.com.smartorderbeorderdomain.order.service.CartItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cartItems")
+@RequiredArgsConstructor
+public class CartItemController {
+    @Autowired
+    private final CartItemService cartItemService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CartItem> getCartItemById(@PathVariable Long id) {
+        return ResponseEntity.ok(cartItemService.getCartItemById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CartItem>> getAllCartItems() {
+        return ResponseEntity.ok(cartItemService.getAllCartItems());
+    }
+
+    @PostMapping
+    public ResponseEntity<CartItem> createCartItem(@RequestBody CartItem cartItem) {
+        return ResponseEntity.ok(cartItemService.createCartItem(cartItem));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CartItem> updateCartItem(@PathVariable Long id, @RequestBody CartItem cartItemDetails) {
+        return ResponseEntity.ok(cartItemService.updateCartItem(id, cartItemDetails));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCartItem(@PathVariable Long id) {
+        cartItemService.deleteCartItem(id);
+        return ResponseEntity.noContent().build();
+    }
+}
